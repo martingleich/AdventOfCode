080ea39f72d007f391a29e3b0c8b709ae44f5711
Martin Gleich <martin.gleich3@web.de>
2022-11-24 17:49:01 +0100

    Day20

---
+++ AdventOfCode/2015/Day20.cs
@@ -0,0 +1,46 @@
+﻿using ProblemsLibrary;
+using System;
+
+namespace AdventOfCode._2015
+{
+    [Problem("2015-20-01")]
+    public class Day20_Part1
+    {
+        [TestCase("50", 4)]
+        [TestCase("130", 8)]
+        public int Execute(string input)
+        {
+            var minDivisorSum = int.Parse(input) / 10;
+            var houses = new int[minDivisorSum + 1];
+            for(int i = 1; i < houses.Length; ++i)
+            {
+                for (int j = i; j < houses.Length; j += i)
+                {
+                    houses[j] += i;
+                }
+                if (houses[i] >= minDivisorSum)
+                    return i;
+            }
+            throw new Exception();
+        }
+    }
+    [Problem("2015-20-02")]
+    public class Day20_Part2
+    {
+        public int Execute(string input)
+        {
+            var minDivisorSum = int.Parse(input) / 11;
+            var houses = new int[minDivisorSum + 1];
+            for(int i = 1; i < houses.Length; ++i)
+            {
+                for (int j = i; j < Math.Min(50 * i,houses.Length); j += i)
+                {
+                    houses[j] += i;
+                }
+                if (houses[i] >= minDivisorSum)
+                    return i;
+            }
+            throw new Exception();
+        }
+    }
+}
