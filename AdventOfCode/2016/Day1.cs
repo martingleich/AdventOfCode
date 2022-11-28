aaeab9f98f3e88b33032b5a6eb9bcc4a14d0c1d1
Martin Gleich <martin.gleich3@web.de>
2022-11-28 21:45:08 +0100

    cleanup

--- AdventOfCode/2016/Day1.cs
+++ AdventOfCode/2016/Day1.cs
@@ -5,34 +5,28 @@ using System.Linq;
 
 namespace AdventOfCode._2016
 {
-    [Problem("2016-01-01", MethodName =nameof(ExecutePart1))]
-    [Problem("2016-01-02", MethodName =nameof(ExecutePart2))]
+    [Problem("2016-01-01", MethodName = nameof(ExecutePart1))]
+    [Problem("2016-01-02", MethodName = nameof(ExecutePart2))]
     public class Day1
     {
-        private enum Direction
+        private record struct Direction(int X, int Y)
         {
-            North,
-            East,
-            South,
-            West,
+            public Position Move(Position position, int count) => new(position.X + X * count, position.Y + Y * count);
+            public Direction Rotate(Direction dir) => new(X * dir.X - Y * dir.Y, X * dir.Y + Y * dir.X);
+            public static readonly Direction Forward = new(1, 0);
+            public static readonly Direction Backward = new(-1, 0);
+            public static readonly Direction Left = new(0, -1);
+            public static readonly Direction Right = new(0, 1);
         }
-        private static Direction Rotate(Direction dir, int steps) => (Direction)(((int)dir + steps) % 4);
         private record struct Position(int X, int Y)
         {
+            public static readonly Position Origin = default;
             public int Length => Math.Abs(X) + Math.Abs(Y);
-            public Position Steps(Direction dir, int count) => dir switch
-            {
-                Direction.North => this with { Y = Y + count },
-                Direction.South => this with { Y = Y - count },
-                Direction.East => this with { X = X + count },
-                Direction.West => this with { X = X - count },
-                _ => throw new NotImplementedException(),
-            };
         }
-        private record struct Step (int ClockwiseSteps, int Distance);
+        private record struct Step(Direction Direction, int Distance);
         private static readonly Func<string, Step?> StepParser = Utilities.MakeRegexParserStruct(@"(R|L)(\d+)", m =>
             new Step(
-                m.Groups[1].Value == "L" ? 3 : 1,
+                m.Groups[1].Value == "L" ? Direction.Left : Direction.Right,
                 int.Parse(m.Groups[2].ValueSpan))
         );
         private static readonly Func<string, IEnumerable<Step>> InputParser = input => input.Split(',').Select(x => StepParser(x.Trim())!.Value);
@@ -40,16 +34,16 @@ namespace AdventOfCode._2016
         {
             public State Move(Step step)
             {
-                var newDir = Rotate(Dir, step.ClockwiseSteps);
-                return new(newDir, Pos.Steps(newDir, step.Distance));
+                var newDir = Dir.Rotate(step.Direction);
+                return new(newDir, newDir.Move(Pos, step.Distance));
             }
             public IEnumerable<State> MoveStepwise(Step step)
             {
-                var newDir = Rotate(Dir, step.ClockwiseSteps);
+                var newDir = Dir.Rotate(step.Direction);
                 var newPos = Pos;
-                for(int i = 0; i < step.Distance; ++i)
+                for (int i = 0; i < step.Distance; ++i)
                 {
-                    newPos = newPos.Steps(newDir, 1);
+                    newPos = newDir.Move(newPos, 1);
                     yield return new(newDir, newPos);
                 }
             }
@@ -59,18 +53,18 @@ namespace AdventOfCode._2016
         [TestCase("R5, L5, R5, R3", 12)]
         public static int ExecutePart1(string input)
         {
-            var pos = InputParser(input).Aggregate(new State(default, default), (state, step) => state.Move(step)).Pos;
+            var pos = InputParser(input).Aggregate(new State(Direction.Forward, Position.Origin), (state, step) => state.Move(step)).Pos;
             return pos.Length;
         }
-        
+
         [TestCase("R8, R4, R4, R8", 4)]
         public static int ExecutePart2(string input)
         {
-            var visisted = new HashSet<Position>() { default };
-            var state = new State(default, default);
+            var visisted = new HashSet<Position>() { Position.Origin };
+            var state = new State(Direction.Forward, Position.Origin);
             foreach (var step in InputParser(input))
             {
-                foreach(var stepState in state.MoveStepwise(step))
+                foreach (var stepState in state.MoveStepwise(step))
                 {
                     if (!visisted.Add(stepState.Pos))
                         return stepState.Pos.Length;
